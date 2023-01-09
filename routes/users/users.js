b7cea3961b4bd4ca54b24d6c85febba93f475980
claudelkros <desiretaira@gmail.com>
2023-01-09 22:22:04 +0100

    no

--- routes/users/users.js
+++ routes/users/users.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+// const bcrypt = require("bcryptjs");
+// const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
 const request = require('request');
 const querystring = require('querystring');
